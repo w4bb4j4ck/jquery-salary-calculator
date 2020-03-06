25f900ffe47246f26a7a0132a8839f94368d8421
w4bb4j4ck <blyblz5@gmail.com>
2020-03-06 16:52:30 -0600

    Mode checkpoint 2

--- client.js
+++ client.js
@@ -4,14 +4,14 @@ let totalMonthly = 0;
 
 function readyNow(){
     $('#submitButton').on('click', submitEmployee);
-    $('#submitButton').on('click', submitEmployee);
+    $('#employeeList').on('click', '#removeButton', removeEmployee);
     displayTotalMonthly();
 }
 
 function submitEmployee(event){
     event.preventDefault();
     let el = $('#employeeList');
-    el.append(`<tr><td>${$('#firstName').val()}</td>
+    el.append(`<tr id="test"><td>${$('#firstName').val()}</td>
     <td>${$('#lastName').val()}</td><td>${$('#id').val()}</td>
     <td>${$('#title').val()}</td><td>${$('#annualSalary').val()}</td>
     <td><button id='removeButton'>Remove</button></td></tr>`);
@@ -28,4 +28,14 @@ function displayTotalMonthly(){
     let el = $('#total');
     el.empty();
     el.append(totalMonthly);
+    if(totalMonthly > 20000){
+        $('#total').addClass('red');
+    }
+    else{
+        $('#total').removeClass('red');
+    }
+}
+
+function removeEmployee(){
+    $('#removeButton').parentsUntil('tr').remove();
 }
